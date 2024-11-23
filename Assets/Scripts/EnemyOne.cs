f1b2af10aa06fd3f27e20a29520a3f5e91f2c711
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-11-23 13:52:37 +0800

    finish enemy one action logic

--- Assets/Scripts/EnemyOne.cs
+++ Assets/Scripts/EnemyOne.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 public class EnemyOne : MonoBehaviour
 {
     bool isHit = false;
+    bool startAction = false;
     public GameObject bullet;
 
     void Update()
     {
         if (isHit)
         {
-            //stop action for 3 seconds
+            StopAllCoroutines();
+            StartCoroutine(PauseThree());
         }
-        if (!isHit)
+        else if (!isHit && !startAction)
         {
+            startAction = true;
             //jump 2 times, shoot 3 bullets, then repeat
+            StartCoroutine(EnemyOneAction());
         }
     }
 
+    IEnumerator EnemyOneAction()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            transform.position += new Vector3(0.3f, 0f, 0f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
+        startAction = false;
+    }
 
+    IEnumerator PauseThree()
+    {
+        yield return new WaitForSeconds(3);
+        isHit = false;
+        startAction = false;
+    }
 }
